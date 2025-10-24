acbd88ce6eab4c172b74d3d80a8f4e7b6a2e1947
Saigeetha21 <geethachandaka21@gmail.com>
2025-10-24 19:38:22 +0530

    adding mergelist

--- src/interviewQues/com/TargetSum.java
+++ src/interviewQues/com/TargetSum.java
@@ -33,6 +33,15 @@ public class TargetSum {
 		List<Integer> nums = Arrays.asList(1,3,2,3,2,4,5,6,1);
         List<Integer> res1 = nums.stream().filter(n->nums.indexOf(n)==nums.lastIndexOf(n)).collect(Collectors.toList());
         System.out.println(res1);
+
+       //adding merge list
+		List<Integer> list1= Arrays.asList(1,9,5,3,7);
+		List<Integer> list2 = Arrays.asList(10,6,8,4,2);
+		Stream<Integer> newStream = Stream.concat(list1.stream(), list2.stream());
+		
+		
+		List<Integer>newList = newStream.sorted().collect(Collectors.toList());
+		System.out.println(newList);
 	}
 
 }
