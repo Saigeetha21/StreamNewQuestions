64bf75f2326cea6de8f0cba3a2c24bb6abc7fa27
Saigeetha@21 <geethachandaka21@gmail.com>
2025-10-24 19:34:00 +0530

    adding non repeated code

--- src/interviewQues/com/TargetSum.java
+++ src/interviewQues/com/TargetSum.java
@@ -4,6 +4,7 @@ package interviewQues.com;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class TargetSum {
 
@@ -15,7 +16,23 @@ public class TargetSum {
 				.filter(b->a+b == target&&!a.equals(b))
 				.map(b-> new int[] {a,b})).findFirst();
 		res.ifPresent(pair -> System.out.println(pair[0]+","+pair[1]));
-
+		
+		
+		
+		
+		
+		String str = "geetha";
+		//str.char()- convert string into ascii values
+		//mapToOjt - convert int into character
+		//first and last index of the letter is same(it means not repeated).
+		List<Character> resu = str.chars().mapToObj(c->(char)c).
+	                    filter(c->str.indexOf(c) == str.lastIndexOf(c)).
+	                    collect(Collectors.toList());
+		System.out.println(resu);
+		
+		List<Integer> nums = Arrays.asList(1,3,2,3,2,4,5,6,1);
+        List<Integer> res1 = nums.stream().filter(n->nums.indexOf(n)==nums.lastIndexOf(n)).collect(Collectors.toList());
+        System.out.println(res1);
 	}
 
 }
